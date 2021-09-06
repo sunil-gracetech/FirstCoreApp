bfa5903f477ea60d689b5559ead35b0e05df4bc1
sunil.gracetech@gmail.com <sunil.gracetech@gmail.com>
2021-09-06 10:06:23 +0530

    Add project files.

---
+++ FirstCoreApp/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            string message = "i am dynamic message";
+            ViewBag.msg = message;
+            ViewData["msg"] = message;
+
+            List<string> courses = new List<string>()
+            {
+                "php",
+                "MVC",
+                "Python",
+                "C sharp",
+                "SQL"
+            };
+
+            ViewBag.courses = courses;
+
+
+
+
+            return View();
+        }
+    }
+}
